573aa9bbbe2f6931768570dec36ec5be285bea6b
Antonio Vilchez <antoniovmonge@gmail.com>
2021-07-29 14:42:10 +0200

    no dbc

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 from pages import (
     overview,
     reviewsModel1,
